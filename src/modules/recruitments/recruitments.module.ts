56f8516e3c32ce32815c50e97545136cd353529a
ohryuuuu <tv9310@kakao.com>
2024-11-23 19:53:46 +0900

    fix : recruitments module

--- src/modules/recruitments/recruitments.module.ts
+++ src/modules/recruitments/recruitments.module.ts
@@ -4,12 +4,12 @@ import { TypeOrmExModule } from 'src/config/typeorm/typeorm-ex.module';
 import { UsersRepository } from '../users/users.repository';
 import { RecruitmentsRepository } from './recruitments.repository';
 import { RecruiterProfilesRepository } from '../recruiter-profiles/recruiter-profiles.repository';
-import { RecruiterProfilesService } from '../recruiter-profiles/recruiter-profiles.service';
 import { RecruiterProfilesModule } from '../recruiter-profiles/recruiter-profiles.module';
+import { RecruitmentsService } from './recruitments.service';
 
 @Module({
   imports : [TypeOrmExModule.forCustomRepository([UsersRepository, RecruiterProfilesRepository, RecruitmentsRepository]), RecruiterProfilesModule],
-  providers: [RecruiterProfilesService],
+  providers: [RecruitmentsService],
   controllers: [RecruitmentsController],
 })
 export class RecruitmentsModule {}
